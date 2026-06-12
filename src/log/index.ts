8074bde772c6541deafd239df9dd73e7b6df417e
Rajmund Paczkowski <rajmund.paczkowski@gmail.com>
2026-06-12 16:07:11 +0200

    WIP

---
+++ src/log/index.ts
@@ -0,0 +1,6 @@
+declare function logDebug(this: void, text: string): void;
+declare function logError(this: void, text: string): void;
+declare function logInfo(this: void, text: string): void;
+declare function logWarning(this: void, text: string): void;
+
+export { logDebug, logError, logInfo, logWarning };
